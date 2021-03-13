3cc12599d4e114a3e3e3269d86607c7d4c136f91
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-03-13 01:54:13 -0800

    03CallStack and Execution stack

---
+++ JavascriptLessons/Javascript/03CallStackExecutionContextExplainedWithLiveExample.js
@@ -0,0 +1,23 @@
+﻿var GlobalEC = "added";
+
+function a() {
+    var fa = "2nd function o/p1"
+    console.log(fa);
+    debugger;
+    b();
+    console.log("lets see when I will be on console");
+}
+
+function b() {
+    debugger;
+    console.log("2nd function o/p2");
+    c();
+}
+
+function c() {
+    debugger;
+    console.log("3rd function 0/p3");
+}
+
+debugger;
+a();
